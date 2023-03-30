6b177f6abd9b251d412801af06bd0179b29702af
stickkman <stickkman@gmail.com>
2023-03-29 20:09:26 -0400

    finished thoughtRoutes, all routes finished

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,1 +1,37 @@
 // Thought Routes
+
+const router = require('express').Router();
+
+const { 
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController');
+
+router // route for api/thoughts '/' 
+    .route('/') 
+    .get(getAllThoughts) 
+    .post(createThought);
+
+router // route for 'api/thoughts/:thoughtId'
+    .route('/:thoughtId')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+router // route for 'api/thoughts/:thoughtId/reactions'
+    .route('/:thoughtId/reactions')
+    .post(addReaction);
+
+router // route for 'api/thoughts/thoughtId/reactions/:reactionId'
+    .route('/:thoughtId/reactions/:reactionId')
+    .delte(deleteReaction);
+      
+
+
+module.exports = router;
+
