31ba6320a27d9cf9e325dd16f3bb75ac22ef74d7
stickkman <stickkman@gmail.com>
2023-03-30 13:55:04 -0400

    coded all CRUD in userController, still needs add/delete friend

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,67 @@
 // User Controller
+
+const { User, Thought} = require('../models');
+
+const userController = {
+
+// GET ALL USERS
+    getAllUsers(req, res) {
+        User.find({})
+        .populate({path: 'thoughts', select: '-__v'})
+        .populate({path: 'friends', select: '-__v'})
+        .select('-__v')
+        .sort({_id: -1})
+        .then(dbUserData => res.json(dbUserData))
+        .catch(e => {console.log(e); res.status(400).json(e);
+        });
+    },
+
+// GET SINGLE USER BY ID
+    getUserById({params}, res) {
+        User.findOne({_id: params.userId})
+        .populate({path: 'thoughts', select: '-__v'})
+        .populate({path: 'friends', select: '-__v'})
+        .then(dbUserData => { 
+            if (!dbUserData) {res.status(404).json({message: '! User Id Not Found !'}); return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(e => {console.log(e); res.status(400).json(e);
+        });
+    },
+
+// CREATE A NEW USER
+    createUser(req, res) {
+        User.create(req.body)
+        .then(dbUserData => res.json(dbUserData))
+        .catch(e => {console.log(e); res.status(400).json(e);
+        });
+    },
+
+// UPDATE USER BY ID
+    updateUser({params, body}, res) {
+        User.findOneAndUpdate({_id: params.userId},
+        {$set: body }, {new: true, runValidators: true}) // set used to only update body field
+        .then(dbUserData => { 
+            if (!dbUserData) {res.status(404).json({message: '! User Id Not Found !'}); return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(e => {console.log(e); res.status(400).json(e);
+        });
+    },
+
+// DELETE USER BY ID
+    deleteUser({params}, res) {
+        User.findOneAndDelete({_id: params.userId})
+        .then(dbUserData => { 
+            if (!dbUserData) {res.status(404).json({message: '! User Id Not Found !'}); return;
+            }
+            return Thought.delteMany({_id: {$in: dbUserData.thoughts}});
+        })
+        .then(() => { res.json({message: '! User and connected thoughts have been deleted !'});
+        })
+        .catch(e => {console.log(e); res.status(400).json(e);
+        });
+    },
+    
